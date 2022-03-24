8ff295be22a47f135a1cbbbebbcc482a3a7b7b61
Victor <victorluiz.bsi@gmail.com>
2022-03-24 16:37:41 -0300

    First Commit

---
+++ Url Shortener/shortener.py
@@ -0,0 +1,9 @@
+import urllib.request
+
+def tiny_url(url):
+    apiurl = "http://tinyurl.com/api-create.php?url="
+    tinyurl = urllib.request.urlopen(apiurl + url).read()
+    return tinyurl.decode("utf-8")
+
+url = input ("Enter your url to shorten :")
+print(tiny_url(url))
