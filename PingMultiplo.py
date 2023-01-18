c45252e4506159233b9dfb28f09da406adcbc0a7
vtvictor <victorluiz.bsi@gmail.com>
2023-01-18 12:02:00 -0300

    first commit

---
+++ PingMultiplo.py
@@ -0,0 +1,11 @@
+import os
+import time
+
+with open ('ping_test_hosts.txt') as file:
+    lerConteudo = file.read()
+    lerConteudo = lerConteudo.splitlines()
+
+    for ip in lerConteudo:
+        print("Verificando o ip: ", ip)
+        os.system(f'ping -n 2 {ip} ')
+        time.sleep(5)
